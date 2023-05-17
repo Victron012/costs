e4c6fd4118c1e4374f8508972c5e8233e01066dc
Victron012 <victron.dafonte@gmail.com>
2023-05-17 10:35:31 -0300

    Criando a home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './components/pages/Home'
-import Company from './components/pages/Company'
-import Contact from './components/pages/Contact'
-import NewProject from './components/pages/NewProject'
+import Home from './components/pages/Home/Home'
+import Company from './components/pages/Company/Company'
+import Contact from './components/pages/Contact/Contact'
+import NewProject from './components/pages/NewProject/NewProject'
 import Container from './components/layouts/Container/Container'
 import NavBar from './components/layouts/NavBar/NavBar'
 import Footer from './components/layouts/Footer/Footer'
-import Projects from './components/pages/Projetcts'
+import Projects from './components/pages/Projects/Projetcts'
 
 
 function App() {
