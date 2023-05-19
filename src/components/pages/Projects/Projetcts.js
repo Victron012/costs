9707ccb9610f6f942184d5f40667f534ae6ddefc
Victron012 <victron.dafonte@gmail.com>
2023-05-19 14:08:57 -0300

    Criando Dashboard

--- src/components/pages/Projects/Projetcts.js
+++ src/components/pages/Projects/Projetcts.js
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom"
 import Message from "../../layouts/Message/Message"
+import styles from './Projetcts.module.css'
+import Container from '../../layouts/Container/Container'
+import LinkButton from '../../../components/layouts/LinkButton/LinkButton'
 
 function Projects(){
 
@@ -10,10 +13,15 @@ function Projects(){
     }
 
     return (
-        <div>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
             <h1>Meus Projetos</h1>
+            <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
             {message && <Message msg={message} type="success"/>}
-            
+            <Container customClass="start">
+                <p>Projetos....</p>
+            </Container>
         </div>
     )
 }
